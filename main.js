72725a5c327c46c67eed7e65875d37bfce07f93e
Bennett Wiredu <benne@Bennetts-MacBook-Air.local>
2024-03-20 17:30:42 -0600

    dot

---
+++ main.js
@@ -0,0 +1,12 @@
+const reading = [
+    ["Designing Data-Intensive Applications", "https://www.oreilly.com/library/view/designing-data-intensive-applications/9781491903063/"],
+    ["Algorithmica - Algorithms for Modern Hardware", "https://en.algorithmica.org/hpc/"],
+    ["Linux Programming Interface", "https://man7.org/tlpi/"],
+    ["Operating Systems: Three Easy Pieces", "https://pages.cs.wisc.edu/~remzi/OSTEP/"],
+]
+
+const watching = [
+    ["Performance Engineering of Software Systems", "https://ocw.mit.edu/courses/6-172-performance-engineering-of-software-systems-fall-2018/"],
+    ["Programming Parallel Computers", "https://www.youtube.com/@JukkaSuomela/playlists"],
+    ["Performance Ninja Class", "https://github.com/dendibakh/perf-ninja"],
+]
